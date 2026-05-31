297ad060e827dc4c75590b2ab0a7178dd34d8449
diddsza78 <131178057+diddsza78@users.noreply.github.com>
2026-05-31 14:34:19 +0200

    page diagnostique

---
+++ app/(mainlayout)/iq/diagnostic/page.tsx
@@ -0,0 +1,13 @@
+import IqDiagnosticPage from "../../iq-diagnostic/page";
+
+export const dynamic = "force-dynamic";
+
+export const metadata = {
+  title: "Diagnostic QI | brainspark",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
+export default IqDiagnosticPage;
