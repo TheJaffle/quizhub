6e3a8b77ff2cb07e2fe0a285248df535246e52c1
diddsza78 <131178057+diddsza78@users.noreply.github.com>
2026-06-13 12:59:42 +0200

    Add freelogictest domain routing

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,22 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [{ type: "host", value: "freelogictest.com" }],
+        destination: "/iq/sondage-light",
+        permanent: true,
+      },
+      {
+        source: "/",
+        has: [{ type: "host", value: "www.freelogictest.com" }],
+        destination: "/iq/sondage-light",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
